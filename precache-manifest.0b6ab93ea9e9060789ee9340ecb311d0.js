b8e3b71b153b4051cea7c52e95e86c29d68c58b5
Guilherme <guilherme.ferreira28@outlook.com>
2019-09-10 10:27:53 +0200

    Updates

---
+++ precache-manifest.0b6ab93ea9e9060789ee9340ecb311d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "723be91fa245e2adcbe68e9f14819b84",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "6171d21a3684549f6a2a",
+    "url": "/ReactApp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "36212995a8b017648ed4",
+    "url": "/ReactApp/static/js/2.cd930777.chunk.js"
+  },
+  {
+    "revision": "6171d21a3684549f6a2a",
+    "url": "/ReactApp/static/js/main.a54e78d9.chunk.js"
+  },
+  {
+    "revision": "567f86e985300b6d8933",
+    "url": "/ReactApp/static/js/runtime~main.f9627fb0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
